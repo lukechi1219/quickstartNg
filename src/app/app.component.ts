1a5aad1b6f47e6596fdac7f5caccf1c5fefb2234
Luke Chi <luke.chi@gmail.com>
2017-03-12 14:29:00 +0800

    fix karma test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,10 @@ export class AppComponent {
 
   ngOnInit() {
     let url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
-    this.data = this.http.get(url).map(x => x.json());
+    this.http.get(url)
+      .map(x => x.json())
+      .subscribe(
+        (data) => this.data = data,
+        (err) => console.error(err));
   }
 }
