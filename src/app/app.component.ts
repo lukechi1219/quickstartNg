cee243fe8501d198ef11c344658f3895e54617a2
Luke Chi <luke.chi@gmail.com>
2017-03-12 14:32:42 +0800

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    let url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
+    const url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
     this.http.get(url)
       .map(x => x.json())
       .subscribe(
