0ea0a5e5fc788595066d5eaba295c84c0a0e2bd3
Luke Chi <luke.chi@gmail.com>
2017-03-12 12:32:32 +0800

    add files

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Http} from '@angular/http';
+import { Component } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  data: any;
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    let url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
+    this.data = this.http.get(url).map(x => x.json());
+  }
+}
