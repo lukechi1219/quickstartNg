419081a01e6357b002ee7a1b6169ef3596c885eb
Luke Chi <luke.chi@gmail.com>
2017-03-13 16:26:59 +0800

    impl race list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,73 @@
 import {Http} from '@angular/http';
-import { Component } from '@angular/core';
+import {Component, PipeTransform, Injectable, Pipe} from '@angular/core';
 import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    './main_style.css'
+  ]
 })
 export class AppComponent {
   title = 'app works!';
-  data: any;
+  races: Race[];
 
   constructor(private http: Http) {
   }
 
   ngOnInit() {
-    const url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
+    const url = '/member/act/queryRaceData?raceVersion=-1&raceCloseVersion=-1&locationSettingVersion=-1&siteRaceVersion=-1&toteVersion=-1&resultVersion=-1&gameType=-1';
+//    const url = '/opendata/datalist/apiAccess?scope=resourceAquire&rid=0b544701-fb47-4fa9-90f1-15b1987da0f5';
+
     this.http.get(url)
       .map(x => x.json())
       .subscribe(
-        (data) => this.data = data,
-        (err) => console.error(err));
-  }
+        (data) => this.handleRaceData(data),
+        (err) => console.error(err)
+      );
+    }
+
+    handleRaceData(data) {
+
+      if (data.status == '200') {
+        this.races = data.races;
+
+        this.races.sort((a,b) => {
+          return a.minutesToRun - b.minutesToRun;
+        });
+
+        for (var i=0; i < this.races.length; i++) {
+          var race = this.races[i];
+
+          if (race.gameType == 0) {
+            race.gameTypeClass = 'HS';
+          } else if (race.gameType == 1) {
+            race.gameTypeClass = 'Dog';
+          } else if (race.gameType == 2) {
+            race.gameTypeClass = 'Har';
+          }
+        }
+      }
+    }
+ }
+
+export class Race {
+  id: number;
+  simpleRaceDate: string;
+  raceType: string;
+  locationID: number;
+  raceNO: number;
+  raceTime: string;
+  countryName: string;
+  locationName: string;
+  gameType: number;
+  gameTypeClass:string;
+  status: number;
+  flagCssClass: string;
+  minutesToRun: number;
+//  "hasResults": true,
+//  "weather": "OCAST",
+//  "distance": "1730",
 }
